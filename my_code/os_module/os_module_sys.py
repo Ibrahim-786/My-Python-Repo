d648eca3fa02a4236e75f06ea391619d55aa460d
Ibrahim-786 <ibrahimpasha112@gmail.com>
2021-04-20 01:19:08 +0530

    added os.sys python code

---
+++ my_code/os_module/os_module_sys.py
@@ -0,0 +1,17 @@
+""" this file is all about sys commonds like instead of using python os.getcwd or
+    os.listdir() we can use directly os dependent commonds like
+    ls pwd in linux
+    dir cwd in windows   """
+
+
+import os
+
+
+x= os.system("dir")
+
+y= os.system("cls")
+
+if (not (x or y)):
+    print("givin commonds found in os")
+else:
+    print("givin commonds not found")
